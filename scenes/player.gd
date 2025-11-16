70477d556c390ebb8e2ec424996d0dd6e06fb93f
SilentPrincess108 <rosan.syal@gmail.com>
2025-11-16 01:24:54 -0500

    added labels for vars

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,7 +4,11 @@ extends CharacterBody2D
 const SPEED = 500.0
 const JUMP_VELOCITY = -500.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player: CharacterBody2D = $"."
 
+func ready():
+	#set starting position
+	player.position = Vector2(115, 613)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,6 +25,10 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 		animated_sprite.play("walking")
+		if direction == 1:
+			animated_sprite.flip_h = false
+		elif direction == -1:
+			animated_sprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_sprite.play("idle")
