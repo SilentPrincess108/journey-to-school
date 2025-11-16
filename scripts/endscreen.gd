b8325c5b005f18f59aa28d6eb6ff3ee9e968ebb2
SilentPrincess108 <rosan.syal@gmail.com>
2025-11-16 04:02:16 -0500

    finished end screens

--- scripts/endscreen.gd
+++ scripts/endscreen.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var grade: AnimatedSprite2D = $grade
 @onready var baldi: AnimatedSprite2D = $baldi
+@onready var energy: Label = $energy
+@onready var result: Label = $result
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,6 +16,10 @@ func _process(delta: float) -> void:
 	if Globals.energy >= 30:
 		grade.play("pass")
 		baldi.play("happy")
+		energy.text = "Energy: High " + "(" + str(Globals.energy) + ")"
+		result.text = "PASS"
 	else:
 		grade.play("fail")
 		baldi.play("angry")
+		energy.text = "Energy: Low " + "(" + str(Globals.energy) + ")"
+		result.text = "FAIL"
