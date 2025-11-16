c9852b02d514fe92f0fd488ec028e5b716da9c5e
SilentPrincess108 <rosan.syal@gmail.com>
2025-11-16 03:23:02 -0500

    created base endscreen

--- scripts/endscreen.gd
+++ scripts/endscreen.gd
@@ -1,6 +1,5 @@
 extends Node
 
-@onready var ui: CanvasLayer = $UI
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,9 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	ui.get_node("energy").text = "Energy: " + str(Globals.energy)
-	
-
-
-func _on_portal_body_entered(body: CharacterBody2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/endscreen.tscn")
+	pass
