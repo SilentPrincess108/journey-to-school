d7b0b682c7abcf7c98257a4ccfca28e27f084449
SilentPrincess108 <rosan.syal@gmail.com>
2025-11-15 20:15:27 -0500

    fixed parallex bg and added boundries

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 1000.0
+const SPEED = 500.0
 const JUMP_VELOCITY = -500.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -19,7 +20,9 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite.play("walking")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("idle")
 
 	move_and_slide()
